7428d1c73cf64063be37d8297d0888512868c3fd
Alex Wang <alex.wang@bgis>
2018-09-18 08:22:35 -0400

    add new models

---
+++ src/Web/Models/UserHome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace dotfusion.web.Models
+{
+    public class UserHome
+    {
+        public IdentityUser User { get; set; }
+
+        public IList<IdentityUser> Teachers { get; set; }
+
+        public IList<Course> Courses { get; set; }
+    }
+}
