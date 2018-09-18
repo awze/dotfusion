cb2921a36b0ffb3cb436717487e5cfff3327bcb9
Alex Wang <alex.wang@bgis>
2018-09-18 09:23:46 -0400

    Add teacher model

---
+++ src/Web/Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace dotfusion.web.Models
+{
+    public class Teacher : IdentityUser<Teacher>
+    {
+        public bool IsTeacher { get; }
+    }
+}
